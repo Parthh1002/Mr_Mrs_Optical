df5af7f84c8e961f3654950f1c6aa9af1941aa0d
aapnorasto <parthh1002@gmail.com>
2026-07-25 14:41:46 +0530

    Optimize Lenis inertia smooth scrolling for buttery smooth 60/120fps feel

--- src/components/layout/SmoothScroll.tsx
+++ src/components/layout/SmoothScroll.tsx
@@ -4,7 +4,19 @@ import { ReactLenis } from 'lenis/react';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.2, smoothWheel: true }}>
+    <ReactLenis
+      root
+      options={{
+        duration: 1.2,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        orientation: 'vertical',
+        gestureOrientation: 'vertical',
+        smoothWheel: true,
+        wheelMultiplier: 1.0,
+        touchMultiplier: 1.6,
+        infinite: false,
+      }}
+    >
       {children as any}
     </ReactLenis>
   );
